2d52ce2278b9e7174ab45a5b7af148c99afac4e9
heyitsaamir <ajawaid191@gmail.com>
2025-08-04 09:40:15 -0700

    Call MCP Directly

--- apps/github-agent/src/index.ts
+++ apps/github-agent/src/index.ts
@@ -3,6 +3,8 @@ import { App } from '@microsoft/teams.apps';
 import { DevtoolsPlugin } from '@microsoft/teams.dev';
 import { McpClientPlugin } from '@microsoft/teams.mcpclient';
 import { OpenAIChatModel } from '@microsoft/teams.openai';
+import { Client } from '@modelcontextprotocol/sdk/client/index.js';
+import { StreamableHTTPClientTransport } from '@modelcontextprotocol/sdk/client/streamableHttp.js';
 
 const app = new App({
   plugins: [new DevtoolsPlugin()],
@@ -22,13 +24,67 @@ app.on('activity', async (ctx) => {
   }
 });
 
-// Gerneral message handler
-app.on('message', async ({ send, activity, signin, next }) => {
-  console.log('Received message:', activity.text);
+app.message(/mcp-direct/, async ({ send, activity, signin, log }) => {
   const userToken = await signin();
   if (!userToken) {
-    console.error('User token is not available. Please sign in first.');
+    log.error('User token is not available. Please sign in first.');
     return;
+  }
+
+  const transport = new StreamableHTTPClientTransport(new URL('https://api.githubcopilot.com/mcp/'), {
+    requestInit: {
+      headers: {
+        "Authorization": `Bearer ${userToken}`
+      },
+    },
+  })
+
+  const client = new Client(
+    {
+      name: 'github-agent',
+      version: '0.1.0',
+    },
+  );
+
+  try {
+    await client.connect(transport);
+    const result = await client.callTool({
+      name: 'assign_copilot_to_issue',
+      arguments: {
+        issueNumber: 3,
+        owner: 'heyitsaamir',
+        repo: 'agent-central',
+      },
+    });
+
+    await send(`Tool call result: ${JSON.stringify(result)}`);
+  } catch (e) {
+    log.error(`Error calling tool assign_copilot_to_issue for issueNumber 3:`, e);
+    throw e;
+  } finally {
+    await client.close();
+  }
+});
+
+app.message(/signout/, async ({ signout, isSignedIn, send }) => {
+  if (!isSignedIn) {
+    await send('You are not signed in.');
+    return;
+  }
+  await signout();
+  await send('You have been signed out.');
+});
+
+// Gerneral message handler
+app.on('message', async ({ send, activity, signin, signout, next, log, ...rest }) => {
+  log.info('Received message:', activity.text);
+  log.info('Other data:', rest['other_data']);
+  const userToken = await signin();
+  if (!userToken) {
+    log.error('User token is not available. Please sign in first.');
+    return;
+  } else {
+    log.info('User token is available:', userToken);
   }
 
   const chatPrompt = new ChatPrompt({
